f8d86c5cf0cc0eebcccdb8605ed28f47989512df
kura59 <ak26b.250.s@gmail.com>
2021-07-13 01:04:16 +0900

    💄 質問テンプレートのレイアウト修正

--- src/components/molecules/TemplateModal.jsx
+++ src/components/molecules/TemplateModal.jsx
@@ -6,23 +6,28 @@ import {
   ModalBody,
   ModalCloseButton,
   Divider,
+  Textarea,
 } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
-import TextareaAutosize from "react-textarea-autosize";
 
 const TemplateModal = (props) => {
   const { isOpenTemplate, onCloseTemplate, title, where, what, why, how } =
     props;
   const [questionText, setQuestionText] = useState("");
+  const [rowsText, setRowsText] = useState();
 
   useEffect(() => {
     const template = `${title}についての質問
-${where ? where + "において" : ""}${what}という問題に対する確認
-${why ? "原因として" + why + "と考えられる" : "原因は不明"}
+${where ? where + "において\n" : ""}${what}
+という問題に対する確認
+${why ? "原因として\n" + why + "\nと考えられる" : "原因は不明"}
 ${
-  how ? "そのため、" + how + "という解決策を考えている" : "解決策がわからない"
+  how
+    ? "そのため、\n" + how + "\nという解決策を考えている"
+    : "解決策がわからない"
 }`;
     setQuestionText(template);
+    setRowsText(template.split("\n").length);
   }, [isOpenTemplate]);
 
   return (
@@ -33,17 +38,15 @@ ${
         <ModalCloseButton />
         <Divider />
         <ModalBody pt={4}>
-          <TextareaAutosize
-            style={{
-              width: "100%",
-              paddingTop: "8px",
-              paddingBottom: "8px",
-              paddingLeft: "16px",
-              paddingRight: "16px",
-              resize: "none",
-            }}
+          <Textarea
+            overflow="hidden"
+            resize="none"
             value={questionText}
-            onChange={(e) => setQuestionText(e.target.value)}
+            onChange={(e) => {
+              setQuestionText(e.target.value);
+              setRowsText(e.target.value.split("\n").length);
+            }}
+            rows={rowsText}
           />
         </ModalBody>
       </ModalContent>
